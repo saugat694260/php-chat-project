a816ac5e41579d7e6a6eb4f67c246e10d610afa5
saugat694260 <khadkasaugat321@gmail.com>
2024-03-16 17:41:00 +0545

    only small features left

--- liveUserUpdate.php
+++ liveUserUpdate.php
@@ -13,58 +13,13 @@ $query="select * from users_data where not id={$user_data['id']}";
 $result=$conn->query($query);
 if($result->num_rows>0){
     while($row=$result->fetch_assoc()){?>
-        <p ><a href="conversations.php? id=<?php echo $row["id"]; ?>"><?php echo $row["username"];  ?><span><?php
-        
-        //update message in realtime
-
-
-}
-?>
-        
-        
-        </span></a></p>
-    <?php
-    }
+        <p ><a href="conversations.php? id=<?php echo $row["id"]; ?>"><?php echo $row["username"];
+    }}
 ;
 ?>
-?>
+
 <script>
     /**
-     *  try
-    {
-        $query="select * from `{$row["id"]}"."{$user_data['id']}` ORDER BY id DESC limit 1;";
-        $result=$conn2->query($query);
-            if($result->num_rows>0){
-                while($row=$result->fetch_assoc()){
-                    if($row['id']==$user_data['id']){echo 'sent:';}
-                    else {echo 'recived:';}
-                 echo "{$row['message']}";
-                }
-  
-             }
-
-    }
-    catch( mysqli_sql_exception)
-    {
-
-        //second try catch
-    try{
-
-        $query="select * from `{$user_data['id']}"."{$row["id"]}` ORDER BY id DESC limit 1;";
-        $secondResult=$conn2->query($query);
-            if($secondResult->num_rows>0){
-                while($row=$secondResult->fetch_assoc()){
-                    if($row['id']==$user_data['id']){echo 'sent:';}
-                    else {echo 'recived:';}
-                    echo "{$row['message']}";
-                    
-                }
-            }
-
-            }catch(mysqli_sql_exception){
-
-               echo "";
-            }
-    }
+     * 
      */
 </script>
