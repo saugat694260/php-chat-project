588bf3ce8cf64b094b312970da00118305dd2560
saugat694260 <khadkasaugat321@gmail.com>
2024-03-21 21:35:59 +0545

    send and recive message

--- ajaxForLiveUpdate/liveUserUpdate.php
+++ ajaxForLiveUpdate/liveUserUpdate.php
@@ -24,7 +24,133 @@ $result=$conn->query($query);
 if($result->num_rows>0){
     while($row=$result->fetch_assoc()){?>
        <div class="user-status"><div class="user-info"><div class="profile-pic-image-container"><img src="userProfiles/<?php if(isset($row['userProfile'])){echo "{$row['userProfile']}";} else{echo "profilePicture.png";} ?>" alt="wryy"></div><a href="conversations.php? id=<?php echo $row["id"]; ?>"><?php echo $row["username"];
-   ?></div><span class="offline-or-online-status" <?php if($row['onlineStatus']=='online'){ echo 'style="background-color:green"';} ?>></span> </div><?php }
+   ?>
+   <span style="position:absolute;left:0px;">
+
+
+
+<?php
+   try
+               {
+                 
+                $query2="select * from `{$user_data['id']}"."{$row['id']}` order by id desc limit 1";
+                $result2=$conn2->query($query2);
+                    if($result2->num_rows>0){
+                     
+                        while($row2=$result2->fetch_assoc()){
+                          
+                            
+                            ?><span style="width:50px ;position:absolute;right:0px;text-align: right;">
+                           
+                            <?php
+                        
+                            if($user_data['id']==$row2['userId']){ 
+                                
+                                echo 'sent';
+                            }
+                            else{
+                                echo 'recived';
+                              
+                            }
+                            
+                            ?>
+                           
+                            </span><span style="<?php
+                            
+                            if($user_data['id']==$row2['userId']){ 
+                                
+                                echo "color:black;font-weight:normal;position:absolute;right:0px;";
+                            }
+                            else{
+                              
+                                echo "color:black;font-weight:bold;position:absolute;right:0px;";
+                            }
+                            ?>"><?php
+                           ?>
+
+                            
+                            <?php
+                                 echo $row2['message'];
+                           
+                        }
+                           ?> </span><?php
+                      
+                     
+                  }
+                  else{
+                  
+                  }
+      
+                }
+               catch( mysqli_sql_exception)
+               {
+           
+                   //second try catch
+               try{
+           
+                $query2="select * from `{$row['id']}"."{$user_data['id']}` order by id desc limit 1";
+                $result2=$conn2->query($query2);
+                    if($result2->num_rows>0){
+                     
+                        while($row2=$result2->fetch_assoc()){
+                          
+                            
+                            ?><span style="width:50px ;position:absolute;right:0px;text-align: right;">
+                            <?php
+                            if($user_data['id']==$row2['userId']){ 
+                                
+                                echo 'sent';
+                            }
+                            else{
+                                echo 'recived';
+                              
+                            }
+                            
+                            ?>
+                           
+                            </span><span style="<?php
+                            
+                            
+                            if($user_data['id']==$row2['userId']){ 
+                                
+                                echo "color:black;font-weight:normal;position:absolute;right:0px;";
+                            }
+                            else{
+                                
+                                echo "color:black;font-weight:bold;position:absolute;right:0px;";
+                            }
+                            
+                            ?>"<?php
+                         ?>
+                            
+                            ><?php
+                           echo $row2['message'];
+                            }
+                            ?></span><?php
+                           
+                      
+                     
+                  }
+                  else{
+                  
+                  }
+      
+                       }catch(mysqli_sql_exception){
+           
+                      } }
+                      
+                      
+               ?></span>
+
+
+
+
+
+
+
+
+</span>
+</div><span class="offline-or-online-status" <?php if($row['onlineStatus']=='online'){ echo 'style="background-color:green"';} ?>></span> </div><?php }
 
 }
 ;
